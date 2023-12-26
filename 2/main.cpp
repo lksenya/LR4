7cec29450ecd7a9046f1dd6cae3bd527d07798c1
lksenya <145290315+lksenya@users.noreply.github.com>
2023-12-26 22:38:59 +0300

    Update main.cpp

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,46 +1,36 @@
-#include <iostream>
-#include <cctype>
-#include <locale>
-#include "modalphacipher.h"
-#include <codecvt>
-#include <typeinfo>
+/**
+ * @file main.cpp
+ * @brief Основной файл программы для взаимодействия с классом Cipher
+ */
 
-using namespace std;
+#include "cipher.h"
 
-void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
-{
-    try {
-        wstring cipherText;
-        wstring decryptedText;
-        modalphacipher cipher(key);
-        cipherText = cipher.encrypt(Text); // зашифровывание
-        if (destructCipherText) // надо "портить"?
-            cipherText.front() = tolower(cipherText.front()); // "портим"
-        decryptedText = cipher.decrypt(cipherText); // расшифровывание
-        wcout<<L"key="<<key<<endl;
-        wcout<<Text<<endl;
-        wcout<<cipherText<<endl;
-        wcout<<decryptedText<<endl;
-    } catch (const cipher_error & e) {
-        wcerr<<"Error: "<<e.what()<<endl;
+int main() {
+    int key;
+    std::cout << "Введите ключ: ";
+    std::cin >> key;
+
+    // Создание объекта класса Cipher с использованием введенного ключа
+    Cipher cipher(key);
+
+    std::string text;
+    std::cout << "Введите текст: ";
+    std::cin >> text;
+
+    int sw;
+    std::cout << "Для шифрования введите - 1 \nДля расшифровки введите - 2 -->";
+    std::cin >> sw;
+
+    // Выбор между шифрованием и расшифровкой в зависимости от введенного значения sw
+    if (sw == 1){
+        std::cout << "Зашифрованный текст: " << cipher.encrypt(text) << std::endl;
     }
-}
+    else if (sw == 2){
+        std::cout << "Расшифрованный текст: " << cipher.decrypt(text) << std::endl;
+    }
+    else {
+        std::cout << "Введено некорректное значение для операции." << std::endl;
+    }
 
-// проверка, чтобы строка состояла только из прописных букв
-bool isValid(const wstring& s)
-{
-    for(auto c:s)
-        if (!iswalpha(c) || !iswupper(c))
-            return false;
-    return true;
-}
-int main()
-{
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    check(L"ПРИВЕТ",L"ПРОЩАЙ");
-    check(L"ПРОЩАЙ",L"");
-    check(L"ПРОЩАЙ",L"ПРИВЕТ321");
-    check(L"П Р О Щ А Й",L"ПРИВЕТ");
-    check(L"321",L"ПРИВЕТ");
+    return 0;
 }
